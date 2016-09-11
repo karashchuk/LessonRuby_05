fd5ef77f592de49a20b7095531c48775e5baf295
karashchuk <karashchuk@inbox.ru>
2016-09-11 20:46:12 +0300

    ДЗ с валидатором

--- lesson_05_2_Dicegame.rb
+++ lesson_05_2_Dicegame.rb
@@ -6,28 +6,49 @@
 # •	Также необходимо проверять валидна ли ставка (число очков соответствует возможному варианту очков которое может выпасть у кубиков, количество кредитов ставки не привышает общее количество кредитов пользователя)
 # •	По умолчанию выигрыш пользователя составляет 25% от его ставки (требуется предусмотреть простую возможность изменения этой величины)
 # •	По завершении игры выводятся результаты с указанием общей суммы выигрыша/проигрыша в кредитах для каждого игрока
+# module Validations
+#   def validuser? user
+#     User===user
+#   end
+#
+# end
+require_relative 'validator'
+
 module DiceGame
+  include Validations
+  QUANTITY = 2
+  EDGES = 6
+  WIN = 0.25
+
   class Dice
 
-    QUANTITY = 2
-    EDGES = 6
-    WIN = 0.25
     def initialize
       @qty = QUANTITY
       @edges = EDGES
     end
+
     def to_s
       "Dices: #{@qty} with #{@edges} edges"
     end
+
     def create *args
       @users = []
       for i in args
-        @users.push(i)
+        @users.push(i) if validuser? i
       end
-      #p @users
+      p "Game starts. In this game:"
+      @users.each {|user| puts user}
     end
 
     def turn
+      if valid_bet?
+        new_turn
+      else
+        puts error
+      end
+    end
+
+    def new_turn
       points = 0
       r = Random.new
       (1..@qty).each {|x|points += r.rand(1..@edges)}
@@ -35,16 +56,7 @@ module DiceGame
       p "Wheel of fortune throws: #{points}"
       @users.each do |user|
         # p "User #{user.name} score #{user.score}"
-        if user.score == points
-          does = "wins"
-          user.money += user.credits * (1 + WIN)
-        else
-          does = "lost"
-          user.money -= user.credits
-        end
-        p "User #{user.name} #{does} and has #{user.money}"
-        user.score = nil
-        user.credits = nil
+        wins?(user,points)
       end
     end
 
@@ -61,22 +73,46 @@ module DiceGame
       end
     end
 
+    private
+
+    def wins? user, points
+      if user.score == points
+        does = "wins"
+        user.money += user.credits * (1 + WIN)
+      else
+        does = "lost"
+        user.money -= user.credits
+      end
+      p "User #{user.name} #{does} and has #{user.money}"
+      user.score = 0
+      user.credits = 0
+    end
+
   end
 
   class User
+    #nclude Validations
     attr_accessor :name, :money, :score, :credits
     attr_reader :money_start
     def initialize name:"anonim", money:
       @name = name
       @money = money
       @money_start = money
+      @score = 0
+      @credits = 0
+      @min = QUANTITY
+      @max = QUANTITY * EDGES
     end
     def to_s
       "User #{@name} with #{@money} $"
     end
     def bet score:,credits:
-      @score = score
-      @credits = credits
+      if (valid_score? score) && (valid_credits? credits)
+        @score = score
+        @credits = credits
+      else
+        puts error
+      end
     end
 
   end
@@ -84,12 +120,24 @@ end
 
 include DiceGame
 dg = Dice.new
-u1 = User.new(name:"U1",money:1000)
-u2 = User.new(name:"U2",money:1000)
+u1 = User.new(name:"John",money:1000)
+u2 = User.new(name:"Max",money:1000)
+u3 = User.new(name:"Peter",money:500)
 
-dg.create u1,u2
+dg.create u1,u2,u3,45
 u1.bet(score:4,credits:100)
 u2.bet(score:5,credits:100)
+dg.turn
+u3.bet(score:6,credits:50)
+
+dg.turn
+u1.bet(score:4,credits:100)
+u2.bet(score:5,credits:100)
+u3.bet(score:6,credits:50)
+dg.turn
+u1.bet(score:4,credits:1000)
+u2.bet(score:5,credits:100)
+u3.bet(score:6,credits:50)
 dg.turn
 # u1 = DiceGame::User.new(money:1000)
 dg.finish
